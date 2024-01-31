a05693dd3db94d3290d03707c5f89d257e64dc97
Viktoria <krystal_v@mail.ru>
2024-01-31 08:51:42 +0300

    HW/IOSUI2-1

--- Navigation new/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation new/Navigation/Profile/ProfileHeaderView.swift
@@ -8,13 +8,10 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-    
-    
     //   add imageView
     private lazy var imageView: UIImageView = {
         //    инициализация imageView
         let imageView = UIImageView()
-        
         imageView.layer.cornerRadius = 75
         imageView.image = UIImage(named: "mouse")
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -40,13 +37,9 @@ class ProfileHeaderView: UIView {
         let statusLabel = UILabel()
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
         statusLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-//        if #available(iOS 13.0, *) {
-//            statusLabel.textColor = .label
-//        } else {
-//            statusLabel.textColor = .gray
-//        }
-        return statusLabel
+        statusLabel.text = "Something..."
         
+        return statusLabel
     }()
     // кнопка статуса
     private lazy var statusButton: UIButton = {
@@ -61,7 +54,7 @@ class ProfileHeaderView: UIView {
         statusButton.layer.shadowColor = UIColor.black.cgColor
         statusButton.layer.shadowOpacity = 0.7
         statusButton.clipsToBounds = false
-        statusButton.addTarget(self, action: #selector(ButtonPressed(_:)), for: .touchUpInside)
+        statusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         
         return statusButton
     }()
@@ -69,28 +62,28 @@ class ProfileHeaderView: UIView {
     //    текст статуса
     private lazy var statusText: UITextField = {
         let statusText = UITextField()
-        //        let statusText : String
         statusText.translatesAutoresizingMaskIntoConstraints = false
+        statusText.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        statusText.backgroundColor = .white
+        statusText.textColor = .black
+        statusText.layer.borderColor = UIColor.black.cgColor
+        statusText.layer.borderWidth = 1
+        statusText.layer.cornerRadius = 12
         statusText.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         statusText.placeholder = "Waiting for something..."
-        if #available(iOS 13.0, *) {
-            statusText.textColor = .label
-        } else {
-            statusText.textColor = .black
-        }
-//        statusText.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
-       
+        
         return statusText
     }()
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
     
     private func setupUI() {
@@ -99,7 +92,9 @@ class ProfileHeaderView: UIView {
         addSubview(statusLabel)
         addSubview(statusButton)
         addSubview(statusText)
-        
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             
             imageView.widthAnchor.constraint(equalToConstant: 150),
@@ -110,28 +105,22 @@ class ProfileHeaderView: UIView {
             nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
             nameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15),
             
+            statusLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 30),
+            statusLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15),
+            
             statusText.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15),
             statusText.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -34),
+            statusText.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            statusText.heightAnchor.constraint(equalToConstant: 40),
             
             statusButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16),
             statusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             statusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             statusButton.heightAnchor.constraint(equalToConstant: 50),
-            
         ])
-        
     }
-    @objc private func ButtonPressed(_ sender: Any) {
+    @objc private func buttonPressed(_ sender: Any) {
         statusLabel.text = statusText.text
-       
+        print()
     }
-//    @objc private func statusTextChanged(_ sender: Any) {
-//        
-//        guard let textField = sender as? StatusText,
-//              let statusText = textField.text else {
-//            print("enter status")
-//            return
-//        }
-//        self.statusLabel = statusText
-//    }
 }
--- Navigation new/Navigation/Profile/ProfileViewController.swift
+++ Navigation new/Navigation/Profile/ProfileViewController.swift
@@ -8,11 +8,7 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-
-//    private let profileObjects: [ProfileObjects] = [
-//        ProfileObjects(image: .mouse, name: "Wise Mouse"),
-//                       ]
+        
     private let profileHeaderView: ProfileHeaderView = {
         let profileHeaderView = ProfileHeaderView()
         profileHeaderView.backgroundColor = .lightGray
@@ -23,42 +19,21 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         view.backgroundColor = .white
-        
-        viewWillLayoutSubviews()
-        
-        //   зададим frame равный frame корневого view
-        profileHeaderView.frame = view.frame
         
         setupUI()
     }
     
     func   setupUI() {
-        
-//           добавляем profileHeaderView в качестве дочернего класса
-            view.addSubview(profileHeaderView)
-        
+        //           добавляем profileHeaderView в качестве дочернего класса
+        view.addSubview(profileHeaderView)
         let safeAreaGuide = view.safeAreaLayoutGuide
-                
-                NSLayoutConstraint.activate([
-                    profileHeaderView.leadingAnchor.constraint(
-                        equalTo: safeAreaGuide.leadingAnchor,
-                        constant: 1.0
-                    ),
-                    profileHeaderView.trailingAnchor.constraint(
-                        equalTo: safeAreaGuide.trailingAnchor,
-                        constant: -1.0
-                    ),
-                    profileHeaderView.topAnchor.constraint(
-                        equalTo: safeAreaGuide.topAnchor,
-                        constant: 0
-                    ),
-                    profileHeaderView.bottomAnchor.constraint(
-                        equalTo: safeAreaGuide.bottomAnchor,
-                        constant: 0
-                    ),
-                ])
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
+            profileHeaderView.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor),
+        ])
     }
-
 }
